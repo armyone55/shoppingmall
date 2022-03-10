3805028681a7b9a86b479cc0b10c21801224f734
arm <dora@abc.com>
2022-03-10 12:26:08 +0700

    1122

--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyConstant {
 //Genernal
-  static String appName = 'Shoppping Mall';
+  static String appName ='Shoppping Mall';
 
   //Route
   static String routeAuthen = '/auten';
@@ -16,4 +16,26 @@ class MyConstant {
   static String image2 = 'images/image2.png';
   static String image3 = 'images/image3.png';
   static String image4 = 'images/image4.png';
+
+//color
+  static Color primary = Color.fromARGB(0, 202, 11, 11);
+  static Color dart = Color.fromARGB(0, 18, 133, 18);
+  static Color light = Color.fromARGB(0, 37, 65, 26);
+
+  //style
+  TextStyle h1Style() => TextStyle(
+        fontSize: 24,
+        color: dart,
+        fontWeight: FontWeight.bold
+      );
+      TextStyle h2Style() => TextStyle(
+        fontSize: 24,
+        color: dart,
+        fontWeight: FontWeight.w700
+      );
+      TextStyle h3Style() => TextStyle(
+        fontSize: 24,
+        color: dart,
+        fontWeight: FontWeight.normal
+      );
 }
