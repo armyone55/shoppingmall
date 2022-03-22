e1b88d8b7d9e37383afe662b7b56732173106f30
arm <dora@abc.com>
2022-03-22 15:47:02 +0700

    update

--- lib/utility/my_dialog.dart
+++ lib/utility/my_dialog.dart
@@ -7,18 +7,18 @@ import 'package:shoppingmall/widgets/show_image.dart';
 import 'package:shoppingmall/widgets/show_title.dart';
 
 class MyDailog {
-  Future<Null> alertLocationServeice(BuildContext context) async {
+  Future<Null> alertLocationServeice(BuildContext context,String title,String message) async {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
         title: ListTile(
           leading: ShowImage(path: MyConstant.image4),
           title: ShowTitle(
-            title: 'Location Service ปิดอยู่ ?',
+            title: title,
             textStyle: MyConstant().h2Style(),
           ),
           subtitle: ShowTitle(
-            title: 'กรุณาเปิด Location Service ด้วยครับ',
+            title: message,
             textStyle: MyConstant().h3Style(),
           ),
         ),
