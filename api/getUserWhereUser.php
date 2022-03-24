6fa9e07fa15a94dd3f398ae32afedd491af4a834
arm <dora@abc.com>
2022-03-24 10:41:12 +0700

    update

--- api/getUserWhereUser.php
+++ api/getUserWhereUser.php
@@ -18,9 +18,9 @@ if (!$link->set_charset("utf8")) {
 if (isset($_GET)) {
 	if ($_GET['isAdd'] == 'true') {
 				
-		$id = $_GET['id'];
+		$user = $_GET['user'];
 
-		$result = mysqli_query($link, "SELECT * FROM user WHERE id = '$id'");
+		$result = mysqli_query($link, "SELECT * FROM user WHERE user = '$user'");
 
 		if ($result) {
 
